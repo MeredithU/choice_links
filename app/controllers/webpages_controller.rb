f928bf6576a0fbcdb69e60d54b8a0663c9b0298b
Meredith Underell <m.underell@gmail.com>
2013-07-11 20:33:51 -0700

    Creation of fav links

--- app/controllers/webpages_controller.rb
+++ app/controllers/webpages_controller.rb
@@ -5,7 +5,21 @@ class WebpagesController < ApplicationController
   end
 
   def new
+    @webpage = Webpage.new
+  end
 
+  def create
+    @webpage = Webpage.new(params[:webpage])
+    if @webpage.save
+      flash[:notice] = "My awesome link has been created."
+      redirect_to @webpage
+    else
+      #tbd
+    end
+  end
+
+  def show
+    @webpage = Webpage.find(params[:id])
   end
 
 end
