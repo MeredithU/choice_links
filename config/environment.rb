e96454d45658faba29797cbb11cdda7c4ab01d45
Meredith Underell <m.underell@gmail.com>
2013-08-12 13:12:57 -0700

    Added date formatting

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ChoiceLinks::Application.initialize!
+
+
+# Date format
+Time::DATE_FORMATS[:saved_on] = "%a, %B %e, %Y"
